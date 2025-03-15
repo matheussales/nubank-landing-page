9ba7692cb2cda53fc95e67a2df76a318fc489024
Matheus Martins de Sales <m.martins.sales@hotmail.com>
2025-03-15 12:57:53 -0300

    feat: add chakra ui + theme

---
+++ src/components/ui/provider.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { ChakraProvider, createSystem, defaultConfig, defaultSystem, defineConfig } from "@chakra-ui/react"
+import {
+  ColorModeProvider,
+  type ColorModeProviderProps,
+} from "./color-mode"
+
+const config = defineConfig({
+  globalCss: {
+    html: {
+      colorPalette: "purple", 
+    },
+  },
+})
+
+const system = createSystem(defaultConfig, config)
+
+export function Provider(props: ColorModeProviderProps) {
+  return (
+    <ChakraProvider value={system}>
+      <ColorModeProvider {...props} />
+    </ChakraProvider>
+  )
+}
